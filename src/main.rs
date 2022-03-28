33c0e41e73290bc6261be6f6587a5a215225723b
taylor d <taylor.delph@eyecuelab.com>
2022-03-28 15:11:26 -0700

    add loop and Err exceptions

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,29 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
+    loop {        
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You've guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small, sucker"),
-        Ordering::Greater => println!("Too big, ya muck!"),
-        Ordering::Equal => println!("You win! No more mean names!"),
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You've guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small, sucker"),
+            Ordering::Greater => println!("Too big, ya muck!"),
+            Ordering::Equal => {
+                println!("You win! No more mean names!");
+                break;
+            }
+        }
     }
 }
-
