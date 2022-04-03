1d5d08e193a81ca5b830f4af7990a78c71fcfde0
taylor d <taylor.delph@eyecuelab.com>
2022-04-03 10:31:35 -0700

    add comments for me to remember later

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..3);
 
-    println!("The secret number is: {}", secret_number);
+    // next line used for testing purposes only
+    // println!("The secret number is: {}", secret_number);
 
     loop {        
         println!("Please input your guess.");
@@ -18,6 +19,7 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
         
+        // shadow the previous value of guess. Shadowing lets us reuse the guess variable name. The shadow is on the left, the original is on the right. we use trim because read_line requires enter to be pressed and adds a "\n" to the end of the string.
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
